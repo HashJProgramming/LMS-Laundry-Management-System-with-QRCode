5bbc1e1c3ba044d595e7c98dc1bd4a192e93e068
HashJProgramming <JoshuaMarceloAmbalong@gmail.com>
2023-11-17 06:32:39 +0800

    Refactor login form layout

--- login.php
+++ login.php
@@ -59,17 +59,17 @@
                                         <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-person">
                                                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
                                             </svg></div>
-                                        <form class="text-center" action="functions/login.php" method="post">
-                                            <div class="mb-3"><input class="form-control" type="text" name="username" placeholder="Username" value="<?php echo isset($_COOKIE['username']) ? $_COOKIE['username'] : ''; ?>"></div>
-                                            <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password" value="<?php echo isset($_COOKIE['password']) ? $_COOKIE['password'] : ''; ?>"></div>
+                                        <form action="functions/login.php" method="post">
+                                            <div class="mb-3 text-center"><input class="form-control" type="text" name="username" placeholder="Username" value="<?php echo isset($_COOKIE['username']) ? $_COOKIE['username'] : ''; ?>"></div>
+                                            <div class="mb-3 text-center"><input class="form-control" type="password" name="password" placeholder="Password" value="<?php echo isset($_COOKIE['password']) ? $_COOKIE['password'] : ''; ?>"></div>
                                             <div class="mb-3">
                                                 <input class="form-check-input" name="remember" type="checkbox" aria-label="remember" <?php echo isset($_COOKIE['username']) ? 'checked' : ''; ?>>
                                                 <label class="form-check-label text-dark" for="remember">
                                                     Remember me
                                                 </label>
                                             </div>
-                                            <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div>
-                                            <p class="text-muted">Laundry Management System (QRCode)</p>
+                                            <div class="mb-3 text-center"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div>
+                                            <p class="text-muted text-center">Laundry Management System (QRCode)</p>
                                         </form>
                                     </div>
                                 </div>
