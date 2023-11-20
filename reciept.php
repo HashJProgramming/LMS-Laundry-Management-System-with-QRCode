0952f42ba65d1eb971f3fb02ad7094c396b531a1
HashJProgramming <JoshuaMarceloAmbalong@gmail.com>
2023-11-21 03:59:12 +0800

    Fix bug in login functionality

--- reciept.php
+++ reciept.php
@@ -1,25 +1,75 @@
 <?php
+include_once 'functions/authentication.php';
 include_once 'functions/connection.php';
-session_start();
-if (!isset($_SESSION['username'])) {
-    header('location:login.php');
-}
 $id = $_GET['id'];
-$kilo = $_GET['kilo'];
-$type = $_GET['type'];
-$type_price = $_GET['type_price'];
-$products = $_GET['products'];
-$total = $_GET['total'];
 $get_tracking_url = getHostByName(getHostName()) . dirname($_SERVER['PHP_SELF']) . '/tracking.php?id=' . $id;
 
-$sql = 'SELECT Transactions.id, customers.fullname AS fullname
-FROM Transactions 
-JOIN users ON Transactions.user_id = users.id 
-JOIN customers ON Transactions.customer_id = customers.id 
-WHERE Transactions.id = :id';
+function getLaundryReciept(){
+    global $db;
+    global $id;
+    $sql = "SELECT t.id, t.customer_id, t.user_id, t.total, l.kilo, p.price, p.name, c.fullname
+        FROM transactions AS t
+        JOIN laundry AS l ON t.id = l.transaction_id 
+        JOIN prices AS p ON l.type = p.id
+        JOIN users AS u ON t.user_id = u.id
+        JOIN customers AS c ON t.customer_id = c.id
+        WHERE t.id = :id";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    foreach($result as $row){
+        ?>
+        <tr class="font-monospace">
+                    <td class="font-monospace text-start mt-0 mb-0" style="font-size: 10px;"><?= $row['name']?></td>
+                    <th class="font-monospace text-center mt-0 mb-0" style="font-size: 10px;"><?= $row['kilo']?>kg</th>
+                    <th class="font-monospace text-center mt-0 mb-0" style="font-size: 10px;">₱<?= number_format($row['price'], 2)?></th>
+                    <td class="font-monospace text-end mt-0 mb-0" style="font-size: 10px;">₱<?= number_format($row['price'] * $row['kilo'], 2)?></td>
+                </tr>
+        <?php
+    }
+}
+
+function getItemsReciept(){
+    global $db;
+    global $id;
+    $sql = "SELECT e.qty, e.item_id, i.name, i.unit
+        FROM items AS i
+        JOIN expenditures AS e ON i.id = e.item_id
+        WHERE e.transaction_id = :id";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    foreach($result as $row){
+        ?>
+        <tr class="font-monospace">
+                    <td class="font-monospace text-start mt-0 mb-0" style="font-size: 10px;"><?= $row['name']?></td>
+                    <th class="font-monospace text-center mt-0 mb-0" style="font-size: 10px;"><?= $row['unit']?></th>
+                    <td class="font-monospace text-end mt-0 mb-0" style="font-size: 10px;"><?= $row['qty']?></td>
+                </tr>
+        <?php
+    }
+}
+
+$sql = "SELECT t.id, t.total, l.kilo, p.price, c.fullname
+FROM transactions AS t
+JOIN laundry AS l ON t.id = l.transaction_id 
+JOIN prices AS p ON l.type = p.id
+JOIN customers AS c ON t.customer_id = c.id
+WHERE t.id = :id";
 $stmt = $db->prepare($sql);
-$stmt->execute(['id' => $id]);
-$result = $stmt->fetch();
+$stmt->bindParam(':id', $id);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+$total = 0;
+$customer = '';
+
+foreach($result as $row){
+    $total += $row['price'] * $row['kilo'];
+    $customer = $row['fullname'];
+}
 
 
 ?>
@@ -29,7 +79,8 @@ $result = $stmt->fetch();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Laundry</title>
+    <title>LMS</title>
+    <meta name="description" content="LMS - Laundry Management System">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
     <link rel="stylesheet" href="assets/css/bs-theme-overrides.css">
@@ -38,49 +89,118 @@ $result = $stmt->fetch();
     <script src="assets/js/qrious.min.js"></script>
 </head>
 
-<body>
-    <div class="container py-4 py-xl-5">
-        <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 d-xl-flex justify-content-center gutter-y">
-            <div class="col">
-                <div class="card border-primary border-2">
-                    <div class="card-body text-center p-4">
-                        <span class="badge rounded-pill bg-primary position-absolute top-0 start-50 translate-middle text-uppercase">Reciept</span>
-                        <h6 class="text-uppercase text-muted card-subtitle">Total</h6>
-                        <h4 class="display-4 fw-bold card-title">₱<?php echo number_format($total, 2); ?></h4>
-                        <canvas id="qr-code"></canvas>
-                    </div>
-                    <div class="card-footer p-4">
-                        <div id="receipt">
-                            
-                            <ul class="list-unstyled">
-                                <li class="d-flex mb-2"><span class="bs-icon-xs bs-icon-rounded bs-icon-primary-light bs-icon me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-check2-circle">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-                                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"></path>
-                                        </svg></span><span><strong>Customer : <?php echo $result['fullname']; ?></strong></span></li>
-                                <li class="d-flex mb-2"><span class="bs-icon-xs bs-icon-rounded bs-icon-primary-light bs-icon me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-check2-circle">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-                                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"></path>
-                                        </svg></span><span><strong>Kg/Kilo : <?php echo $kilo; ?></strong></span></li>
-                                <li class="d-flex mb-2"><span class="bs-icon-xs bs-icon-rounded bs-icon-primary-light bs-icon me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-check2-circle">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-                                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"></path>
-                                        </svg></span><span><strong>Type : <?php echo $type; ?> -&nbsp;₱<?php echo number_format($type_price, 2); ?></strong></span></li>
-                                <li class="d-flex mb-2"><span class="bs-icon-xs bs-icon-rounded bs-icon-primary-light bs-icon me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-check2-circle">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-                                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"></path>
-                                        </svg></span><span><strong>Products</strong>&nbsp;: <strong>₱<?php echo number_format($products, 2); ?></strong></span></li>
-                                <li class="d-flex mb-2"><span class="bs-icon-xs bs-icon-rounded bs-icon-primary-light bs-icon me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-check-circle">
-                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-                                            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"></path>
-                                        </svg></span><span><strong>Total :&nbsp;₱<?php echo number_format($total, 2); ?></strong></span></li>
-                            </ul>
-                        </div>
-                        <a class="btn btn-primary d-block w-100" role="button" onclick="printReceipt()">Print</a>
-                        <a class="btn btn-primary d-block w-100" role="button" href="transaction.php" style="margin-top: 10px;">Go back</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+<body onload="printPageAndRedirect()">
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="font-monospace text-center" style="color: var(--bs-gray-900);font-size: 13px;">
+                    <img src="assets/img/washing-clothes.gif" width="40">&nbsp;Laundry Management System<br>
+                    <span style="font-weight: normal !important;">Street Unknown, Pagadian City</span><br>
+                    <span style="font-weight: normal !important;">Phone (+63) 000-000-000</span><br>
+                    <span style="font-weight: normal !important;">Date: <?php echo date('Y-m-d')?></span><br>
+                    <canvas class="mt-1 mb-2 text-center" id="qr-code"></canvas>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr></tr>
+                <tr></tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-borderless">
+            <thead>
+                <tr>
+                    <th class="font-monospace text-center" style="font-size: 15px;">Laundry Reciept</th>
+                </tr>
+            </thead>
+            <tbody class="font-monospace">
+                <tr class="font-monospace"></tr>
+                <tr class="font-monospace"></tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="table-responsive font-monospace">
+        <table class="table table-borderless">
+            <thead class="font-monospace">
+                <tr class="font-monospace">
+                    <th class="font-monospace" style="font-size: 15px;"><span style="font-weight: normal !important;">CUSTOMER: <strong><?php echo $customer; ?></strong></span></th>
+                    <th class="font-monospace text-end" style="font-size: 15px;"></th>
+                    <th class="font-monospace text-end" style="font-size: 15px;"></th>
+                    <th class="font-monospace text-end" style="font-size: 15px;">INVOICE #<?php echo $_GET['id'] ?></th>
+                </tr>
+            </thead>
+            <tbody class="font-monospace">
+                
+            </tbody>
+        </table>
+    </div>
+    <div class="table-responsive font-monospace">
+        <table class="table table-borderless">
+            <thead class="font-monospace">
+                <tr class="font-monospace">
+                    <th class="font-monospace text-center" style="font-size: 15px;"><span style="font-weight: normal !important;">LAUNDRY</span></th>
+                </tr>
+            </thead>
+            <tbody class="font-monospace">
+                
+            </tbody>
+        </table>
+    </div>
+    <div class="table-responsive font-monospace">
+        <table class="table table-borderless">
+            <thead class="font-monospace">
+                <tr class="font-monospace">
+                    <th class="font-monospace text-start" style="font-size: 12px;"><span><strong>TYPE</strong></span></th>
+                    <th class="font-monospace text-center" style="font-size: 12px;"><span><strong>LOAD</strong></span></th>
+                    <th class="font-monospace text-center" style="font-size: 12px;"><span><strong>PRICE</strong></span></th>
+                    <th class="font-monospace text-end" style="font-size: 12px;"><span><strong>TOTAL</strong></span></th>
+                </tr>
+            </thead>
+            <tbody class="font-monospace">
+                <?php getLaundryReciept()?>
+            </tbody>
+        </table>
+    </div>
+    <div class="table-responsive font-monospace">
+        <table class="table table-borderless">
+            <thead class="font-monospace">
+                <tr class="font-monospace">
+                    <th class="font-monospace text-center" style="font-size: 15px;"><span style="font-weight: normal !important;">ITEMS</span></th>
+                </tr>
+            </thead>
+            <tbody class="font-monospace">
+               
+            </tbody>
+        </table>
+    </div>
+    <div class="table-responsive font-monospace">
+        <table class="table table-borderless">
+            <thead class="font-monospace">
+                <tr class="font-monospace">
+                    <th class="font-monospace text-start" style="font-size: 12px;"><span><strong>ITEM</strong></span></th>
+                    <th class="font-monospace text-center" style="font-size: 12px;"><span><strong>UNIT</strong></span></th>
+                    <th class="font-monospace text-end" style="font-size: 12px;"><span><strong>QTY</strong></span></th>
+                </tr>
+            </thead>
+            <tbody class="font-monospace">
+                <?php getItemsReciept() ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="table-responsive">
+        <table class="table">
+            <thead class="font-monospace">
+                <tr class="font-monospace">
+                    <th class="font-monospace text-end"><strong>TOTAL</strong>&nbsp;<strong>₱<?php echo number_format($total, 2); ?></strong></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr></tr>
+            </tbody>
+        </table>
     </div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
@@ -94,55 +214,26 @@ $result = $stmt->fetch();
     <script src="assets/js/buttons.print.min.js"></script>
     <script src="assets/js/listTable.js"></script>
     <script src="assets/js/theme.js"></script>
-
     <script>
         $(document) .ready(function() {
             (function() {
             var qr = new QRious({
                         element: document.getElementById('qr-code'),
-                        size: 200,
+                        size: 150,
                         value: '<?php echo $get_tracking_url; ?>'
                     });
                 })();
+
         } );
-
-
-
-    function printReceipt() {
-        var printWindow = window.open('', '_blank');
-        printWindow.document.open();
-        printWindow.document.write('<html><head>');
-        printWindow.document.write('<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">');
-        printWindow.document.write('</head><body>');
-        printWindow.document.write('<div class="card border-primary border-2 mt-1">');
-        printWindow.document.write('<div class="card-body text-center p-4">');
-        printWindow.document.write('<span class="badge rounded-pill bg-primary position-absolute top-0 start-50 translate-middle text-uppercase">Reciept</span>');
-        printWindow.document.write(' <h6 class="text-uppercase text-muted card-subtitle">Total</h6>');
-        printWindow.document.write(' <h4 class="display-4 fw-bold card-title">₱<?php echo $total; ?></h4>');
-        printWindow.document.write('<img id="qr-code-img" src="" />');
-        printWindow.document.write('</div>');
-        printWindow.document.write('<div class="card-footer p-4">');
-        printWindow.document.write('');
-        printWindow.document.write(document.getElementById('receipt').innerHTML);
-        printWindow.document.write('</div>');
-        printWindow.document.write('</body></html>');
-
-        var qrValue = '<?php echo $get_tracking_url; ?>';
-        var qrCodeImg = printWindow.document.getElementById('qr-code-img');
-
-        var qr = new QRious({
-            value: qrValue,
-            size: 200
-        });
-
-        qrCodeImg.src = qr.toDataURL();
-
-        setInterval(function() {
-            printWindow.close();
-        }, 10);
-        printWindow.document.close();
-        printWindow.print();
-    }
+            function printPageAndRedirect() {
+                setTimeout(function() {
+                    window.setTimeout(function() {
+                        window.print();
+                        window.location.href = 'transaction.php';
+                    }, 500);
+                }, 500);
+            }
+            
     </script>
 </body>
 
