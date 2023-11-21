b789a40caaffdbe92ced9c2c21a2df0924459d81
HashJProgramming <JoshuaMarceloAmbalong@gmail.com>
2023-11-21 16:35:50 +0800

    Add favicon to LMS and tracking pages

--- reciept.php
+++ reciept.php
@@ -80,6 +80,7 @@ foreach($result as $row){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>LMS</title>
+    <link rel="shortcut icon" href="assets/img/washing-clothes.gif" type="image/gif">
     <meta name="description" content="LMS - Laundry Management System">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
--- tracking.php
+++ tracking.php
@@ -26,6 +26,7 @@ $result = $stmt->fetchAll();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Laundry Management System Tracking</title>
+    <link rel="shortcut icon" href="assets/img/washing-clothes.gif" type="image/gif">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
