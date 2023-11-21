c0a35fc14668c96d64a00bd0d87af9e9aacd3efc
HashJProgramming <JoshuaMarceloAmbalong@gmail.com>
2023-11-21 15:42:17 +0800

    Remove unused JavaScript file and add DataTable functionality

--- supply.php
+++ supply.php
@@ -192,7 +192,6 @@
     <script src="assets/js/vfs_fonts.js"></script>
     <script src="assets/js/buttons.html5.min.js"></script>
     <script src="assets/js/buttons.print.min.js"></script>
-    <script src="assets/js/listTable.js"></script>
     <script src="assets/js/theme.js"></script>
     <script src="assets/js/sweetalert.min.js"></script>
     <script>
@@ -231,6 +230,37 @@
             console.log(id); 
             $('input[name="data_id"]').val(id);
         });
+
+        $(document).ready(function() {
+            $('#dataTable').DataTable( {
+                dom: 'Blfrtip',
+                buttons: [
+                    {
+                        extend: 'excel',
+                        className: 'btn btn-primary',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4]
+                        }
+                    },
+                    {
+                        extend: 'pdf',
+                        className: 'btn btn-primary',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4]
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        className: 'btn btn-primary',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4]
+                        }
+                    }
+                ]
+            } );
+            
+            
+        } );
     </script>
 </body>
 
