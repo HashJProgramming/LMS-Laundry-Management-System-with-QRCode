c7487e6e9c9af3ba9b9ae1b4af1646719bb3da16
HashJProgramming <JoshuaMarceloAmbalong@gmail.com>
2023-11-21 17:16:01 +0800

    Fix indentation in transaction-modal.php

--- functions/modals/transaction-modal.php
+++ functions/modals/transaction-modal.php
@@ -2,22 +2,22 @@
 include_once 'functions/views/get-data.php';
 ?>
 <div class="modal fade" role="dialog" tabindex="-1" id="transaction">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">New Transaction</h4><button class="btn-close" type="button" aria-label="Close" data-bs-dismiss="modal"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="functions/transaction.php" method="post">
-                        <div class="mb-3"><label class="form-label" for="first_name"><strong>Select Customer</strong></label><select class="form-select" name="id">
-                                <optgroup label="Select Customer">
-                                    <?php customer_list(); ?>
-                                </optgroup>
-                            </select></div>                     
-                    
-                </div>
-                <div class="modal-footer"><button class="btn btn-primary" type="submit">Save</button></div>
-                </form>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">New Transaction</h4><button class="btn-close" type="button" aria-label="Close" data-bs-dismiss="modal"></button>
             </div>
+            <div class="modal-body">
+                <form action="functions/transaction.php" method="post">
+                    <div class="mb-3"><label class="form-label" for="first_name"><strong>Select Customer</strong></label><select class="form-select" name="id">
+                            <optgroup label="Select Customer">
+                                <?php customer_list(); ?>
+                            </optgroup>
+                        </select>
+                    </div>
+            </div>
+            <div class="modal-footer"><button class="btn btn-primary" type="submit">Save</button></div>
+            </form>
         </div>
     </div>
+</div>
